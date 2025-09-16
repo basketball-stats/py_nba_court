6f34accc419e0ebc3c6cbc60a546faa8402e50c4
basketball-stats <enr1co+ballgit@protonmail.com>
2025-09-15 19:51:59 -0700

    Fixed center position of 3pt arc
    3pt arc center position moved to 5'3 distance (5.25 ft -> 52.5), see NBA court for reference

--- create_court.py
+++ create_court.py
@@ -4,9 +4,8 @@ def create_court(ax, color):
     ax.plot([-220, -220], [0, 140], linewidth=2, color=color) # 3PT STRAIGHT LINE
     ax.plot([220, 220], [0, 140], linewidth=2, color=color) # 3PT STRAIGHT LINE
     ## Draw the circle as if it was an ellipse. 
-    ## Second element: half of rim inner radius
-    ## Third element of center is: 7 to 10 so that 3pt straight line match 140
-    ax.add_artist(mpl.patches.Arc((0, 40+7.5//2+7), 237.5*2, 237.5*2, theta1=22, theta2=158, 
+    ## Circle center distance from baseline is 5'3 = 5.25 ft
+    ax.add_artist(mpl.patches.Arc((0, 52.5), 237.5*2, 237.5*2, theta1=22, theta2=158, 
                                   facecolor='none', edgecolor=color, lw=2)) # 3PT ARC
     ax.plot([-80, -80], [0, 190], linewidth=2, color=color) # PAINT SIDE
     ax.plot([80, 80], [0, 190], linewidth=2, color=color) # PAINT SIDE
@@ -14,7 +13,7 @@ def create_court(ax, color):
     ##ax.plot([60, 60], [0, 190], linewidth=2, color=color) # inner lines in paint
     ax.plot([-80, 80], [190, 190], linewidth=2, color=color)
     ax.add_artist(mpl.patches.Circle((0, 190), 60, facecolor='none', edgecolor=color, lw=2)) # FT CIRCLE
-    ax.add_artist(mpl.patches.Circle((0, 55), 7.5, facecolor='none', edgecolor=color, lw=2)) # RIM
+    ax.add_artist(mpl.patches.Circle((0, 52.5), 7.5, facecolor='none', edgecolor=color, lw=2)) # RIM
     ax.plot([-30, 30], [40, 40], linewidth=2, color=color) # BACKBOARD
     ax.add_artist(mpl.patches.Arc((0, 470), 60*2, 60*2, theta1=0, theta2=360,facecolor='none',edgecolor=color,lw=2)) # CENTER CIRCLE
     ax.add_artist(mpl.patches.Arc((0, 55), 40*2, 40*2, theta1=0, theta2=180, facecolor='none', edgecolor=color, lw=2)) # PAINT CIRCLE
